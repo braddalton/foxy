cfad0728f34a8549c6a96ea86d18e7a1c80818aa
Brad Dalton <wpsites.net@gmail.com>
2014-07-16 17:49:33 +0700

    Update functions.php

--- functions.php
+++ functions.php
@@ -2,8 +2,8 @@
 register_sidebar( array(
 		'name' => 'After Post',
 		'id' => 'after-post',
-		'before_widget' => '<div id="%1$s" class="fwidget %2$s">',
-		'after_widget' => '</div>',
+		'before_widget' => '<div id="%1$s" class="widget %2$s">',
+		'after_widget' => '</div> <!-- end .widget -->',
 		'before_title' => '<h4 class="widgettitle">',
 		'after_title' => '</h4>',
 	) );
